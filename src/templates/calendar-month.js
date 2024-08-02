f2ca2211a7bb9ed0c1fb8f715b6e23fd0bb0ba9f
Miroslav Smrtic <miro218@gmail.com>
2024-08-02 10:30:36 +0200

    feat: add web-calendar

---
+++ src/templates/calendar-month.js
@@ -0,0 +1,5 @@
+
+    export const calendarMonth = `<h2 class="calendar-dates-month">
+        <strong class="calendar-month-name"></strong> /
+        <span class="calendar-year-name"></span>
+    </h2>`
